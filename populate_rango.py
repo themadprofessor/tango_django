791710e95c864fa3bde0ef47a768aadf165619fb
Stuart <stu@reilly-family.co.uk>
2018-01-24 14:57:59 +0000

    Start fixing code to work with tests

--- populate_rango.py
+++ populate_rango.py
@@ -37,7 +37,7 @@ def populate():
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -52,8 +52,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
