e5b90f26a2b00caa9ceecea27b6b7705d4f30e31
Stuart <stu@reilly-family.co.uk>
2018-01-24 14:45:48 +0000

    Add likes and views to categories Change display list for page on admin page Add tests

--- populate_rango.py
+++ populate_rango.py
@@ -32,9 +32,9 @@ def populate():
          "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
