444f1f6a0492896608ac10cffde167c92d4f9f6b
Stuart Reilly <stu@reilly-family.co.uk>
2018-01-24 11:34:25 +0000

    Add about page using template

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'name': 'Stuart Reilly'}
+    return render(request, 'rango/about.html', context=context_dict)
