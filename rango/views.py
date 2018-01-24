6be5c2293aacb230459be88ccc408dc652c44b83
Stuart Reilly <stu@reilly-family.co.uk>
2018-01-24 11:08:28 +0000

    Add template support Change rango/ to use template

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
