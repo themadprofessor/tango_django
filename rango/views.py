f09481b705648a0a2eaa32a660af2c190907442d
Stuart Reilly <stu@reilly-family.co.uk>
2018-01-25 16:37:39 +0000

    Add db data on index

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
