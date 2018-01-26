c58831486c440cdd7762bbc7fa8ee5e46fc880c0
Stuart Reilly <stu@reilly-family.co.uk>
2018-01-26 17:03:33 +0000

    Add categories info page

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about')
+    url(r'^about/', views.about, name='about'),
+    url(r'^category/(P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category')
 ]
