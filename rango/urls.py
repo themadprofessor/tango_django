b4c451605172782673f49b2b98a83e86e0d7fbe0
Stuart Reilly <stu@reilly-family.co.uk>
2018-01-29 15:09:07 +0000

    Fix category view

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^category/(P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category')
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 
+
 # Create your views here.
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'name': 'Stuart Reilly'}
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
